dc9a8d4b08aebe1c9f42eac5a8436135414cc843
mcbalsa <mcbalsa@protonmail.com>
2023-08-16 15:16:44 -0400

    added splash and navbar
    Added an initial splash screen and navbar with placeholders

---
+++ frontend/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HelloWorld from '../components/HelloWorld.vue';
+import HomePage from '../components/HomePage.vue';
+
+const routes = [
+    {
+        path: '/hello',
+        name: 'HelloWorld',
+        component: HelloWorld
+    },
+    {
+        path: '/',
+        name: 'HomePage',
+        component: HomePage
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+  })
+  
+  export default router
